c3752adc7c25532149d7991bbb85a6f3631e9592
dwmoeller1 <dwmoeller1@aol.com>
2019-02-28 19:41:30 -0600

    Proposed instruction changes
    In particular the fact that the method name must be Sum would be non-obvious to the learner.

--- SumMethod/Program.cs
+++ SumMethod/Program.cs
@@ -11,5 +11,11 @@ namespace SumMethod
 
         // TODO: Create a method that returns the sum of all integer numbers up to and including given value. 
         // Use the equation (n * (n + 1)) / 2.
+        // For example, if the method gets passed a value of 4, it would return the value of 1 + 2 + 3 + 4 
+        // (the equation provided will do this calculation)
+        // The method
+            // must be named Sum
+            // must by public
+            // must take a single parameter
     }
 }
